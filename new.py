5fc751126fb6a43e7199eeb35371b9bd29303fff
d98malik <114065633+d98malik@users.noreply.github.com>
2024-08-03 23:51:46 +0530

    worldbank economic data scrapper project done

--- new.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://webscraper.io/test-sites/e-commerce/allinone"
-
-content = requests.get(url)
-
-soup = BeautifulSoup(content.text, 'lxml')
